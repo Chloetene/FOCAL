17f75e25af1171414b6c90f842b7dabc7f23e700
Richard <rremigoso4310@gmail.com>
2020-05-15 13:38:33 -0500

    changed sample rate to 200 Hz for faster sampling time of 2 seconds

--- maxim.js
+++ maxim.js
@@ -131,9 +131,9 @@ MAX30102.prototype.init = function(){
   this.write8(C.REG_OVF_COUNTER, 0x00);  //OVF_COUNTER[4:0]
   this.write8(C.REG_FIFO_RD_PTR, 0x00);  //FIFO_RD_PTR[4:0]
   
-  this.write8(C.REG_FIFO_CONFIG, 0x4f);  //sample avg = 0, fifo rollover=false, fifo almost full = 3
+  this.write8(C.REG_FIFO_CONFIG, 0x4f);  //sample avg = 4, fifo rollover=false, fifo almost full = 3
   this.write8(C.REG_MODE_CONFIG,0x03);  //0x02 for Red only, 0x03 for SpO2 mode 0x07 multimode LED
-  this.write8(C.REG_SPO2_CONFIG,0x27);  // SPO2_ADC range = 4096nA, SPO2 sample rate (100 Hz), LED pulseWidth (411uS)
+  this.write8(C.REG_SPO2_CONFIG,0x2b);  // SPO2_ADC range = 4096nA, SPO2 sample rate (200 Hz), LED pulseWidth (411uS)
     
   this.write8(C.REG_LED1_PA,0x24);  //Choose value for ~ 7mA for LED1
   this.write8(C.REG_LED2_PA,0x24);  // Choose value for ~ 7mA for LED2
